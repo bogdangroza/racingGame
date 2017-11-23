7a5aa45f45171f5aa5ee1b1b94be1c94560817c4
bogdangroza <grozab92@gmail.com>
2017-11-23 20:39:27 +0200

    simplified App.main

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -6,37 +6,6 @@ public class App {
         Track track = new Track();
         track.setLenght(100.5);
         race.setTrack(track);
-
-
-        Engine engine1 = new Engine();
-        engine1.setManufacturer("Volvo");
-
-        Car car1 = new Car(engine1);
-        car1.setName("Volvo");
-        car1.setColor("black");
-        car1.setFuelLevel(100.0);
-        car1.setMileage(12.5);
-        car1.setDoorCount(4);
-
-
-        Engine engine2 = new Engine();
-        engine2.setManufacturer("Honda");
-
-        Car car2 = new Car(engine2);
-        car2.setName("Honda");
-        car2.setColor("Red");
-        car2.setFuelLevel(100.0);
-        car2.setMileage(13.5);
-        car2.setDoorCount(2);
-
-
-        Vehicle vehicle = new Vehicle();
-        vehicle.accelerate(20.5);
-
-        AutoVehicle autoVehicle = new AutoVehicle(new Engine());
-
-
-
-
+        race.start();
     }
 }
